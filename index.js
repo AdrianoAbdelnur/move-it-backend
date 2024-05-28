36f1af47cdc7501387f11a268d4b0c55f2711ec6
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-05-28 15:52:39 +0930

    SocketIo added in the app

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require('mongoose')
+const http = require('http');
+const socketIo = require('socket.io');
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
@@ -25,9 +27,44 @@ app.use((req, res, next) => {
 
 app.use("/api", require("./src/routes"));
 
+const server = http.createServer(app);
+
+// Integrar Socket.IO con el servidor HTTP
+const io = socketIo(server, {
+    cors: {
+        cors: {
+            origin: '*',
+            methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE']
+        }
+    }
+});
+
+const usuarios = {};
+
+io.on('connection', (socket) => {
+    console.log('Un usuario se ha conectado con ID:', socket.id);
+
+    socket.on('nuevoUsuario',(nombreUsuario)=> {
+        usuarios[nombreUsuario] = socket.id;
+        console.log(`${nombreUsuario} se ha conectado con ID ${socket.id}`);
+        console.log(usuarios)
+    } )
+
+    // Ejemplo de evento de mensaje
+    socket.on('mensaje', (msg) => {
+        console.log('Mensaje recibido:', msg);
+        io.emit('mensaje', msg); // Reenvía el mensaje a todos los clientes
+    });
+
+    // Manejo de desconexión
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado:', socket.id);
+    });
+});
+
 mongoose.connect(process.env.DB_URL).then(() => {
     console.log("conectado a mongodb");
-    server = app.listen(process.env.API_PORT, () => {
+    server.listen(process.env.API_PORT, () => {
         console.log(`aplicacion escuchando en puerto ${process.env.API_PORT}`);
     });
 });
