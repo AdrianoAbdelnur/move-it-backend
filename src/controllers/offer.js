519408a2ebef0648e08c4c774a3cab1de16211d9
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-06-02 00:48:56 +0930

    Populate post in newOffer

--- src/controllers/offer.js
+++ src/controllers/offer.js
@@ -3,8 +3,9 @@ const Offer = require("../models/Offer");
 
 const addOffer = async(req, res) => {
     try {
-        const newOffer = new Offer(req.body)
+        let newOffer = new Offer(req.body)
         await newOffer.save();
+        newOffer = await Offer.findById(newOffer._id).populate('post');
         res.status(200).json({message: 'Offer sent successfully', newOffer})
     } catch (error) {
         res.status(error.code || 500).json({message : error.message})
@@ -14,7 +15,7 @@ const addOffer = async(req, res) => {
 const getMyOffers =  async (req, res) => {
     try {
         const {id} = req.params
-        const myOffers = await Offer.find({owner: id}).populate("owner").populate("post");
+        const myOffers = await Offer.find({owner: id}).populate("post");
         res.status(200).json({message: 'Offers found succesfully', myOffers})
     } catch (error) {
         res.status(error.code || 500).json({message : error.message})
