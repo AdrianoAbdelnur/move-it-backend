7f2c7dc8fca9a7160a014148bbf076910f7f8af3
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-07-07 13:35:49 +0930

    addMessage route

--- src/routes/userPost.js
+++ src/routes/userPost.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { addPost, getMyPosts, getAllPosts, getPendingPosts, addNewOffer, selectOffer, modifyStatus } = require("../controllers/userPost");
+const { addPost, getMyPosts, getAllPosts, getPendingPosts, addNewOffer, selectOffer, modifyStatus, addMessage } = require("../controllers/userPost");
 const router = express.Router();
 
 router.post('/addPost', addPost )
@@ -9,5 +9,6 @@ router.get('/pendingPosts', getPendingPosts )
 router.put('/addNewOffer',  addNewOffer)
 router.patch('/selectOffer',  selectOffer)
 router.patch('/modifyStatus',  modifyStatus)
+router.patch('/addMessage/:postId',  addMessage)
 
 module.exports = router; 
