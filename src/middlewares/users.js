801cbbccc78939f85c43d0651e3bb7219ed5dbbf
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-08-12 15:46:13 +0930

    Message for email already in use updated

--- src/middlewares/users.js
+++ src/middlewares/users.js
@@ -5,7 +5,7 @@ const verifyRegisterFields = () => {
     return [
         body('email').isEmail().normalizeEmail().trim().escape().custom( email => {
             return  User.findOne({ email }).then(user => {
-                if (user) return Promise.reject('e-mail already in use');
+                if (user) return Promise.reject('The email address is already in use. If this email belongs to you, please log in instead');
             })
         }),
         body('password').isLength({ min: 6, max: 16 }),
