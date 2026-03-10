9d1577bb18b5cbcf20ebf8459620befc098a76bf
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2026-03-10 13:36:45 -0300

    Expose server time header for client clock sync

--- index.js
+++ index.js
@@ -128,6 +128,10 @@ app.use(
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.setHeader("x-server-time", String(Date.now()));
+  next();
+});
 
 app.use(cors({
   origin: function (origin, callback) {
